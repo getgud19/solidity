1538b904eba62a418eaa7cee50c6ef6ed9e05b40
getgud <aleksnowak19@gmail.com>
2023-08-22 15:12:35 +0200

    blockchian

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -1,17 +1,17 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {Script} from "forge-std/Script.sol";
-import {FundMe} from "../src/FundMe.sol";
-import {HelperConfig} from "./HelperConfig.s.sol";
+import{Script} from "forge-std/Script.sol";
+import{FundMe} from "../src/FundMe.sol";
+import{HelperConfig} from "./HelperConfig.s.sol";
 
+contract DeployFundMe is Script{
+    function run() external returns(FundMe) {
 
-contract DeployFundMe is Script {
-    function run() external returns (FundMe) {
-        HelperConfig helperConfig = new HelperConfig(); // This comes with our mocks!
-        (address ethUsdPriceFeed) = helperConfig.activeNetworkConfig();
-
+        HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
         vm.startBroadcast();
+        
         FundMe fundMe = new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
         return fundMe;
--- src/PriceConverter.sol
+++ src/PriceConverter.sol
@@ -10,7 +10,6 @@ library PriceConverter {
     function getPrice(AggregatorV3Interface priceFeed) internal view returns (uint256) {
         // Sepolia ETH / USD Address
         // https://docs.chain.link/data-feeds/price-feeds/addresses
-       
         (, int256 answer, , , ) = priceFeed.latestRoundData();
         // ETH/USD rate in 18 digit
         return uint256(answer * 10000000000);
@@ -18,7 +17,8 @@ library PriceConverter {
 
     // 1000000000
     function getConversionRate(
-        uint256 ethAmount, AggregatorV3Interface priceFeed
+        uint256 ethAmount,
+        AggregatorV3Interface priceFeed
     ) internal view returns (uint256) {
         uint256 ethPrice = getPrice(priceFeed);
         uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
